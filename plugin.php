cf8251da7407820330ed2b7ea80542095c5680c2
wokamoto <wokamoto1973@gmail.com>
2012-07-11 17:25:54 +0900

    added widget

--- plugin.php
+++ plugin.php
@@ -27,7 +27,7 @@ License:
     along with this program; if not, write to the Free Software
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
-class my_clip {
+class MyClip {
 	const COOKIE_KEY = 'my_clip';
 	const COOKIE_EXPIRES = 7;
 
@@ -37,8 +37,13 @@ class my_clip {
 			add_action('wp_enqueue_scripts', array(&$this,'add_scripts'));
 			add_action('wp_footer', array(&$this,'footer_scripts'));
 		}
+
+		// register ajax
 		add_action('wp_ajax_clip_search', array(&$this, 'clip_search'));
 		add_action('wp_ajax_nopriv_clip_search', array(&$this, 'clip_search') );
+
+		// register widget
+		add_action('widgets_init', array(&$this, 'register_widget'));
 	}
 
 	public function add_scripts() {
@@ -52,6 +57,15 @@ class my_clip {
         echo "<script>\n";
         echo "
 jQuery(function($){
+  if ( $.cookie('".self::COOKIE_KEY."') ) {
+    $.ajax({
+      type: 'GET',
+      url: '".$ajax_url."',
+      dataType: 'json',
+      success: clip_set,
+    });
+  }
+
   $('.clip_icon').click(function(){
     var clips = $.cookie('".self::COOKIE_KEY."');
     var id=$(this).attr('id').replace('clip-','');
@@ -104,6 +118,70 @@ jQuery(function($){
 		echo json_encode($result);
 	    die();
 	}
+
+	function register_widget() {
+		if ( class_exists('WP_Widget') )
+			register_widget('MyClipWidget');
+	}
 }
 
+/******************************************************************************
+ * CommentersWidgetController Class ( for WP2.8+ )
+ *****************************************************************************/
+if ( class_exists('WP_Widget') ) :
+
+class MyClipWidget extends WP_Widget {
+	function __construct() {
+		$widget_ops = array(
+			'classname' => 'widget_my-clip' ,
+			'description' => 'My Clip',
+			);
+		$this->WP_Widget('my-clip', 'My Clip', $widget_ops);
+	}
+
+	public function widget( $args, $instance ) {
+		extract($args);
+		$title = apply_filters('widget_title', 
+				isset($instance['title']) ? trim($instance['title']) : '' ,
+				$instance ,
+				$this->id_base);
+        echo $before_widget;
+        if ( !empty($title) )
+            echo $before_title . $title . $after_title;
+        echo '<div class="my-clip_wrap"></div>' . "\n";
+        echo $after_widget;
+	}
+
+	public function update( $new_instance, $old_instance ) {
+		$instance = $new_instance;
+		if (isset($new_instance['title']))
+			$new_instance['title'] = strip_tags($instance['title']);
+		return $instance;
+	}
+
+	public function form( $instance ) {
+		$instance = wp_parse_args( (array) $instance, array('title' => '', 'limit' => 5) );
+		$input_format = '<p><label for="%2$s">%1$s</label><input class="widefat" id="%2$s" name="%3$s" type="text" value="%4$s" /></p>'."\n";
+		printf(
+			$input_format ,
+			__('Title:') ,
+			$this->get_field_id('title') ,
+			$this->get_field_name('title') ,
+			esc_attr($title)
+		);
+		printf(
+			$input_format ,
+			__('Limit:') ,
+			$this->get_field_id('limit') ,
+			$this->get_field_name('limit') ,
+			intval($limit)
+		);
+	}
+}
+
+endif;
+
+/******************************************************************************
+ * Go Go Go!
+ *****************************************************************************/
 New my_clip();
