10c3420a77b611f06a0c1e7925ce7f9838792e8f
wokamoto <wokamoto1973@gmail.com>
2012-07-12 16:31:46 +0900

    modified clip js

--- plugin.php
+++ plugin.php
@@ -127,7 +127,7 @@ jQuery(function($){
       var count = 0;
       var ul = $('<ul></ul>');
       var moreclip = false;
-      var hideclip = $(this)).css('display') !== 'none';
+      var hideclip = $('.more-clip', $(this)).css('display') !== 'none';
       $.each(data, function(){
         var li = $('<li id="my-clip-post-' + this.id + '"></li>')
           .append('<a href="' + this.permalink + '">' + this.title + '</a> <a href="#" class="my-clip-remove" id="clipped-' + this.id + '">x</a>');
